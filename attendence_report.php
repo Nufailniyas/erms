d78f30b91ecdc0d5411a047ae091e1c0893981f9
Nufailniyas <Nufailniyas98@gmail.com>
2020-04-14 09:03:59 -0700

    Attendence

---
+++ attendence_report.php
@@ -0,0 +1,50 @@
+<?php
+$title = "Dashboard | Online Examination Result Management System | SLGTI";
+$description = "Online Examination Result  Management System (ERMS)-SLGTI";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once("./head.php"); ?>
+</head>
+
+<body>
+    <div class="page-wrapper toggled bg2 border-radius-on light-theme">
+        <nav id="sidebar" class="sidebar-wrapper">
+            <?php include_once("nav.php"); ?>
+        </nav>
+        <main class="page-content pt-2">
+            <div id="overlay" class="overlay"></div>
+            <div class="container-fluid p-5">
+                <!-- #1 Insert Your Content-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Sam <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Month</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log Out</button>
+    </form>
+  </div>
+</nav>
+
+                <!-- #1 Insert Your Content" -->
+            </div>
+        </main>
+    </div>
+    <?php include_once("script.php"); ?>
+</body>
+
+</html>
